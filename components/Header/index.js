61a9a9d90ad89e1de5945862064c1aabd08b1a27
Diddleslip <diddleslip@gmail.com>
2020-03-13 11:28:21 -0400

    Finished Header and Tabs Section

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,25 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    let header = document.createElement("div"),
+        spanOne = document.createElement("span"),
+        h1 = document.createElement("h1"),
+        spanTwo = document.createElement("span");
+
+        header.classList.add("header");
+        spanOne.classList.add("date");
+        spanTwo.classList.add("temp");
+
+        spanOne.textContent = "SMARCH 28, 2019";
+        h1.textContent = "Lambda Times";
+        spanTwo.textContent = "98°";
+
+        header.append(spanOne, h1, spanTwo);
+
+    return header;
+}
+
+let parent = document.querySelector(".header-container");
+
+parent.append(Header());
